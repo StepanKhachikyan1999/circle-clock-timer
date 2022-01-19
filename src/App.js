0a7608d23cb6199b26a1e6788b4557b99e4086d7
Stepan <stepan.khachikyan@beeoncode.com>
2022-01-19 13:21:05 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import "./App.css"
+
+
+const formatRemainingTime = time => {
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
+  return `${minutes}:${seconds}`;
+};
+
+const renderTime = ({ remainingTime }) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Too lale...</div>;
+  }
+
+  return (
+    <div className="timer">
+        <div className="value">{formatRemainingTime(remainingTime)}</div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        CountdownCircleTimer
+        <br />
+        React Component
+      </h1>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          isPlaying
+          duration={3600}
+          colors={[["#F0BC0B", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+          onComplete={() => [true, 1000]}
         >
-          Learn React
-        </a>
-      </header>
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
+      <p className="info">
+        Change component properties in the code filed on the right to try
+        difference functionalities
+      </p>
     </div>
   );
 }
